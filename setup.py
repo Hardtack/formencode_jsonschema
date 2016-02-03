bd7e6738c01ee23f7966090615d6db316ac8c2ce
Choi Geonu <6566gun@gmail.com>
2016-02-03 17:06:50 +0900

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='formencode_jsonschema',
-    version='0.1.0',
+    version='0.1.1dev',
     description='Dump formencode schema JSON schema using Marshmallow',
     author='Choi Geonu',
     author_email='6566gun@gmail.com',
